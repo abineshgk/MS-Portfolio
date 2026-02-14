d84abbdf330985cfd4e149b58d2ddef3b49e733b
abineshgk <abineshgk2005@gmail.com>
2026-02-14 20:13:37 +0530

    About Update

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,38 +5,41 @@ function About() {
       <div className="about-left">
         <h2>About Me</h2>
 
-        <p>
-          I’m <strong>Abinesh G K</strong>, a motivated and enthusiastic 
-          <strong> Full Stack MERN Developer</strong> and BCA graduate.
-          I have built a strong foundation in frontend and backend development
-          through hands-on real-world projects.
-        </p>
+      <p>
+        I’m <strong>Abinesh G K</strong>, a passionate and enthusiastic 
+        <strong>Full Stack MERN Developer</strong> and BCA graduate. 
+        As a fresher, I have developed a strong foundation in frontend and backend development 
+        through academic learning and hands-on projects.
+       </p>
 
-        <p>
-          I specialize in <strong>React.js</strong> for building modern user interfaces 
-          and <strong>Node.js</strong> for developing scalable backend APIs. 
-          I also have experience with <strong>MySQL</strong> and <strong>MongoDB</strong> 
-          for database integration.
-        </p>
+      <p>
+        I have experience working with <strong>React.js</strong> to build responsive and modern user interfaces 
+        and <strong>Node.js</strong> to develop backend APIs. 
+        I am also familiar with <strong>MySQL</strong> and <strong>MongoDB</strong> 
+        for database design and integration.
+      </p>
 
-        <p>
-          I am currently seeking an entry-level opportunity where I can apply 
-          my technical skills, learn from experienced professionals, and contribute 
-          to impactful software solutions.
-        </p>
+      <p>
+        I am currently seeking an entry-level opportunity where I can apply my skills, 
+        continue learning from experienced professionals, and contribute to meaningful 
+        and innovative software solutions.
+      </p>
+
+
       </div>
 
       <div className="about-right">
         <div className="about-card">
           <h3>Quick Highlights</h3>
 
-          <ul>
-            <li>✔ MERN Stack Developer</li>
-            <li>✔ 6 Months of hands on Projects</li>
-            <li>✔ Understandable in React & REST APIs</li>
-            <li>✔ Database Integration</li>
-            <li>✔ Clean UI & Responsive Design</li>
-          </ul>
+         <ul>
+          <li>✔ MERN Stack Enthusiast</li>
+          <li>✔ Practical Project Experience</li>
+          <li>✔ React.js & Backend API Development</li>
+          <li>✔ Database Handling</li>
+          <li>✔ Responsive & User-Friendly Interfaces</li>
+        </ul>
+
         </div>
       </div>
 
