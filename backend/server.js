2bd055aa546fdd368bf5625d9fb0c0f45249612b
abineshgk <abineshgk2005@gmail.com>
2026-03-16 16:15:04 +0530

    Render

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,47 @@
 require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
-const nodemailer = require("nodemailer")
+const { Resend } = require("resend")
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://your-vercel-domain.vercel.app"
+  ],
+  methods: ["GET","POST"],
+  credentials: true
+}))
+
 app.use(express.json())
+
+const resend = new Resend(process.env.RESEND_API_KEY)
+
+app.get("/", (req,res)=>{
+  res.send("Backend running")
+})
 
 app.post("/contact", async (req,res)=>{
 
-try {
-
 const {name,email,message} = req.body
 
-const transporter = nodemailer.createTransport({
- host: "smtp.gmail.com",
- port: 587,
- secure: false,
- auth: {
-   user: process.env.EMAIL_USER,
-   pass: process.env.EMAIL_PASS
- }
-})
+res.status(200).json({success:true})
 
-await transporter.sendMail({
- from: email,
- to:process.env.EMAIL_USER,
- subject:"New Portfolio Message",
- text:`Name: ${name}
+try {
+
+await resend.emails.send({
+ from: "Portfolio <onboarding@resend.dev>",
+ to: process.env.EMAIL_USER,
+ subject: "New Portfolio Message",
+ text: `Name: ${name}
 Email: ${email}
 Message: ${message}`
 })
 
-res.status(200).json({success:true})
-
 } catch(error){
 
 console.log(error)
-
-res.status(500).json({success:false})
 
 }
 
